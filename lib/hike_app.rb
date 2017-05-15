209342dccc4404bb1b847b1280fc43fe9288c6e1
Aubrey Masten, Kai De Rochemont and kat gifford <kai@>
2017-05-15 11:55:45 -0700

    further connect and test database associations

--- lib/hike_app.rb
+++ lib/hike_app.rb
@@ -5,22 +5,14 @@ class Hike < ActiveRecord::Base
   has_and_belongs_to_many :features
   has_many :hike_ratings
   has_many :ratings, through: :hike_ratings
-  # has_many :hikes_landmarks
-  # has_many :features_hikes
-  # has_many :features, through: :features_hikes
-  # has_many :landmarks, through: :hikes_landmarks
 end
 
 class Landmark < ActiveRecord::Base
   has_and_belongs_to_many :hikes
-  # has_many :hikes_landmarks
-  # has_many :hikes, through: :hikes_landmarks
 end
 
 class Feature < ActiveRecord::Base
   has_and_belongs_to_many :hikes
-  # has_many :hikes_landmarks
-  # has_many :hikes, through: :hikes_landmarks
 end
 
 class Rating < ActiveRecord::Base
@@ -36,13 +28,3 @@ class HikeRating < ActiveRecord::Base
   belongs_to :hike
   belongs_to :rating
 end
-
-# class HikeLandmark < ActiveRecord::Base
-#   belongs_to :hike
-#   belongs_to :landmark
-# end
-#
-# class HikeFeature < ActiveRecord::Base
-#   belongs_to :hike
-#   belongs_to :feature
-# end
