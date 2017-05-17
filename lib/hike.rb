228c66bccb4e6f12e07a289843a53467927cf379
Kaidero <kaiderochemont@gmail.com>
2017-05-17 10:12:32 -0700

    fix weather zip generation (#17)
    * initialize files
    
    * update initialize files
    
    * add database migrations
    
    * add schema to files
    
    * create and test database table associations
    
    * further connect and test database associations
    
    * add template front-end
    
    * create basic layout for index page
    
    * create regions page and hike page and add some basic css styles
    
    * remove ratings table and join table from database schema
    
    * add conversion of lat and lng to zip code
    
    * add weather method to Hike class
    
    * remove hike_app.rb
    
    * test weather method
    
    * Update css styles for hike page
    
    * add CSS styling to footer
    
    * edit further css styling
    
    * plug in database data on hike pages
    
    * fix bug in zipcode generation

--- lib/hike.rb
+++ lib/hike.rb
@@ -6,7 +6,13 @@ class Hike < ActiveRecord::Base
 
   def weather
     geo_result = Geocoder.search("#{self.latitude},#{self.longitude}")
-    zip = geo_result[0].data['address_components'].last['long_name']
+    data_size = geo_result[0].data['address_components'].length
+    target = data_size - 1
+    zip = geo_result[0].data['address_components'][target]['long_name']
+    if /^\d{5}$/.match(zip).nil?
+      target = data_size - 2
+    end
+    zip = geo_result[0].data['address_components'][target]['long_name']
     api_result = RestClient.get "http://api.wunderground.com/api/3df9e5569912899b/geolookup/conditions/q/#{zip}.json"
     jhash = JSON.parse(api_result)
     jhash['current_observation']['weather']
