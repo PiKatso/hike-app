ae53f5152ea77dc90ebf50b2b62147e7aea4de5d
BenWilliams7 <benjarwilliams@gmail.com>
2017-05-18 10:43:06 -0700

    add search by name function (#28)

--- app.rb
+++ app.rb
@@ -41,3 +41,38 @@ end
 get '/search' do
   erb :search
 end
+
+get '/search1' do
+  search = params['search1'].upcase
+  @hikes = Hike.where("upper(name) like ?", "%#{search}%").order("name")
+  erb :search_results
+end
+
+get '/search2' do
+  search = params['search2']
+  erb :search_results
+end
+
+get '/search3' do
+  search = params['search3']
+  @hikes = Hike.where
+  erb :search_results
+end
+
+get '/search4' do
+  search = params['search4']
+  @hikes = Hike.where
+  erb :search_results
+end
+
+get '/search5' do
+  search = params['search5'].upcase
+  @hikes = Hike.where
+  erb :search_results
+end
+
+get '/search6' do
+  search = params['search6']
+  @hikes = Hike.where
+  erb :search_results
+end
