73351b17dcea0c80a834e5fea511f173a514e5eb
PiKatso <PiKatso@users.noreply.github.com>
2017-05-18 11:00:36 -0700

    Kat main (#29)
    * initialize files
    
    * update initialize files
    
    * add database migrations
    
    * add schema to files
    
    * create and test database table associations
    
    * further connect and test database associations
    
    * add template front-end
    
    * create basic layout for index page
    
    * create regions page and hike page and add some basic css styles
    
    * remove ratings table and join table from database schema
    
    * add conversion of lat and lng to zip code
    
    * add weather method to Hike class
    
    * remove hike_app.rb
    
    * test weather method
    
    * Update css styles for hike page
    
    * add CSS styling to footer
    
    * edit further css styling
    
    * plug in database data on hike pages
    
    * fix bug in zipcode generation
    
    * add map to hikes pages
    
    * minor CSS changes
    
    * remove nights from forecast
    
    * add features to hike pages and create route for feature pages
    
    * minor CSS fixes
    
    * refactor random hike feature
    
    * add fisrt two integration specs and update random hike generator
    
    * add integration specs and update routing
    
    * update index layout
    
    * update readme
    
    * add readme/imgs

--- spec/hike_app_integration_spec.rb
+++ spec/hike_app_integration_spec.rb
@@ -12,10 +12,35 @@ describe("the region path", {:type => :feature}) do
   end
 end
 
+describe("the region to hike path", {:type => :feature}) do
+  it("lets the user select a region to select a specific hike from") do
+    visit("/")
+    click_link("Urban Sprawl")
+    click_link("Chicken Creek Loop Hike")
+    expect(page).to have_content('4 Day Forecast')
+  end
+end
+
 describe("the random hike generator path", {:type => :feature}) do
   it("lets the user select be suggested a random hike") do
     visit("/")
     click_link("Random hike")
     expect(page).to have_content('Current Weather')
+  end
+end
+
+describe("the search preferences path", {:type => :feature}) do
+  it("lets the user select their preferences to find matching hikes") do
+    visit("/")
+    click_link("Search")
+    expect(page).to have_content('Which features are you looking for?')
+  end
+end
+
+describe("the search preferences path", {:type => :feature}) do
+  it("lets the user select their preferences to find matching hikes") do
+    visit("/")
+    click_link("Features")
+    expect(page).to have_content('Features')
   end
 end
